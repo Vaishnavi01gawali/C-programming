2a89a61890e03e2cc458ccf34f2608f2395d72d9
Gawali Vaishnavi <gawalivaishnavi449@gmail.com>
2025-08-28 19:28:52 +0530

    Add files via upload

---
+++ 13_logical_AND.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int iNo1=0;
+	int iNo2=1;
+	int iNo3=2;
+	int iAns;
+	
+	iAns=iNo1&&++iNo2&&iNo3++;
+	
+	printf("iNo1&&++iNo2&&iNo3++ answer is = %d%d%d%d",iNo1,iNo2,iNo3,iAns); //iNo1&&++iNo2&&iNo3++ answer is = 0120 
+	
+	return 0;
+}
