6ae5b7bf7a8077c5b5fdae2d078542ec8370a36f
Gawali Vaishnavi <gawalivaishnavi449@gmail.com>
2025-09-08 18:54:03 +0530

    Add files via upload

---
+++ 43_definition_different_datatype_same_scope.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int no=10;          
+//	float no=99.99f;     error 'no' : redefinition; multiple initialization
+	
+	printf("%d",no);  // 10
+	
+	return 0;
+}
