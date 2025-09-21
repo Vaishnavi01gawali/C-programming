a6773179e878006f80d1ebe1d83e7247823a043f
Gawali Vaishnavi <gawalivaishnavi449@gmail.com>
2025-09-21 19:20:35 +0530

    Add files via upload

---
+++ 68_pragma_pack_1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+
+struct demo1
+{
+	char chChar;
+	double dNo;
+}obj1;
+
+#pragma pack(4)
+struct demo2
+{
+	char chChar;
+	double dNo;
+}obj2;
+
+#pragma pack(1)
+struct demo3
+{
+  char chChar;
+  double dNo;	
+}obj3;
+
+#pragma pack()
+struct demo4
+{
+  char chChar;
+  double dNo;	
+}obj4;
+
+int main(void)
+{
+	printf("sizeof obj1 %d\n",sizeof(obj1));    // sizeof obj1 16
+	printf("sizeof obj2 %d\n",sizeof(obj2));    // sizeof obj2 12
+	printf("sizeof obj3 %d\n",sizeof(obj3));    // sizeof obj3 9
+	printf("sizeof obj4 %d\n",sizeof(obj4));    // sizeof obj4 16
+	
+	return 0;
+}
