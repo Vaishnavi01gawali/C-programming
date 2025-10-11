9d194243a38ba927dbb43dfe1e349c34a1b8576f
Gawali Vaishnavi <gawalivaishnavi449@gmail.com>
2025-10-11 22:22:25 +0530

    Add files via upload

---
+++ 95_pointer_addition_with_pointer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int arr[5]={10,20,30,40,50};
+	int *pPtr1=&arr[0];
+	int *pPtr2=&arr[4];
+	
+	printf("%d\n",pPtr1);    //13629444
+	printf("%d\n",pPtr2);    //13629460
+	
+//	printf("%d\n",pPtr1+pPtr2);   Error: '+' : cannot add two pointers
+	
+	return 0;
+}
