148523fbb9ccdc93d460150856a6a26d517576b3
Gawali Vaishnavi <gawalivaishnavi449@gmail.com>
2025-10-13 22:34:27 +0530

    Add files via upload

---
+++ 97_pointer_subtraction_with_pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int arr[5]={10,20,30,40,50};
+	int *pPtr1=&arr[0];
+	int *pPtr2=&arr[4];
+	
+	printf("%d\n",pPtr1);           //10223152
+	printf("%d\n",pPtr2);           //10223168
+	
+	printf("%d\n",pPtr1-pPtr2);    //-4
+	printf("%d\n",pPtr2-pPtr1);     //4
+	
+	return 0;
+}
