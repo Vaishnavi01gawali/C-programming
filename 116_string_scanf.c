609f2dbb706bc8fc10f1355739c02f0cf6c6629b
Gawali Vaishnavi <gawalivaishnavi449@gmail.com>
2025-12-12 22:31:04 +0530

    Add files via upload

---
+++ 116_string_scanf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	char szName[15];
+	
+	printf("Enter your Name :-"); //Enter your Name :-Vaishnavi Gawali
+	scanf("%s",szName);
+	
+	printf("%s",szName);         //Vaishnavi
+	
+	return 0;
+}
