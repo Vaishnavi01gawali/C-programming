4372ea4428987e9407b1d4af3b51654cd28fece0
Gawali Vaishnavi <gawalivaishnavi449@gmail.com>
2025-12-13 22:25:57 +0530

    Add files via upload

---
+++ 117_gets.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	char szName[15];
+	
+	printf("Enter your Name :-");  //Enter your Name :- Vaishnavi Gawali
+	gets(szName);
+	
+	printf("%s",szName);           // Vaishnavi Gawali
+	
+	return 0;
+}
