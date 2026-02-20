1820fa8432f82fc58c6e53ee18aa9b1e785ed0bb
Gawali Vaishnavi <gawalivaishnavi449@gmail.com>
2026-02-20 21:50:47 +0530

    Add files via upload

---
+++ 139_strchr.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+   char chChar;
+   char chStr[20];
+   char *pPtr;
+
+   printf("Enter The String:\t");  // Enter The String:       gauri
+   gets (chStr);
+   
+   printf("\nEnter Character To Be Found:\t");  //Enter The String:       gauri
+   scanf("%c", &chChar);
+
+   pPtr=strchr(chStr,chChar);
+
+   if (pPtr==NULL)
+     printf("\nCharacter is Not Found\n");
+   else
+     printf("\nCharacter is Found At %d Location",(pPtr-chStr)+1); //Character Is Found At 1 Location
+
+  return 0;
+
+}
