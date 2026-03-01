3bd64337ef56b8928a13143ffa3a1f033ca949b0
Gawali Vaishnavi <gawalivaishnavi449@gmail.com>
2026-03-01 20:37:59 +0530

    Add files via upload

---
+++ 142_cla_printing_values.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc,char * argv[])
+{
+	int iCounter;
+	for(iCounter=0;iCounter < argc;iCounter++)
+	{
+	 	printf("%s",argv[iCounter]);       //142_cla_printing_values.exeprogramofprintingvalues
+	}
+	  
+	  return 0;
+}
